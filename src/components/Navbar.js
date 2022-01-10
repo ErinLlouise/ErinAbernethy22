ad57258b34ee8802a3da19815fde7c28bf1eebbf
ErinLlouise <erinllouise3@gmail.com>
2022-01-10 16:32:49 +1300

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { ArrowRightIcon } from "@heroicons/react/solid";
 
 function Navbar() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <header className="bg-gray-800 md:sticky top-0 z-20">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="title-font font-medium text-white mb-4 md:mb-0">
+            <a href="#about" className="ml-3 text-xl">
+              A Portfolio
+            </a>
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a href="#projects" className="mr-5 text-white">
+              Past Work
+            </a>
+            <a href="#skills" className="mr-5 text-white">
+              Skills
+            </a>
+            <a href="#testimonials" className="mr-5 text-white">
+              Testimonials
+            </a>
+          </nav>
+          <a
+            href="#contact"
+            className="text-indigo inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+            Hire Me
+            <ArrowRightIcon className="w-4 h-4 ml-1" />
+          </a>
+        </div>
+      </header>
     )
 }
 
